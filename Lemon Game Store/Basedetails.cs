a4dc276bf39cda8afc58d953365ac2c3e014d977
Troy Joaquin G. Vallarta <58638607+Zofserif@users.noreply.github.com>
2021-04-01 14:08:08 +0800

    Update Basedetails.cs

--- Lemon Game Store/Basedetails.cs
+++ Lemon Game Store/Basedetails.cs
@@ -4,8 +4,10 @@ using System.Text;
 
 namespace Lemon_Game_Store
 {
+    
     class Basedetails
     {
+        //Basedetails is also an abstraction
         // For game details and console
         //  This is class is for inheritance, so that we will not create these get and set for every class
         public string Title { get; set; }
