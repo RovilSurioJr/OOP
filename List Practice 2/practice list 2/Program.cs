fa9b81adf6bf6aa53105e0176933da975da4a8e8
RovilSurioJr <70187981+RovilSurioJr@users.noreply.github.com>
2021-04-01 13:17:01 +0800

    update from lemon game store

--- List Practice 2/practice list 2/Program.cs
+++ List Practice 2/practice list 2/Program.cs
@@ -64,7 +64,7 @@ namespace LemonStore
                 //List<Game> theGames = new List<Game>();
                 Gamelist.Add(new Game
                 {
-                    Title = "Persona 5",
+                    Title = "PERSONA 5",
                     Platform = "Console",
                     Condition = "Brandnew",
                     Price = 1499
@@ -72,7 +72,7 @@ namespace LemonStore
 
                 Gamelist.Add(new Game
                 {
-                    Title = "Uncharted",
+                    Title = "UNCHARTED",
                     Platform = "Console",
                     Condition = "2nd Hand",
                     Price = 500
@@ -80,7 +80,7 @@ namespace LemonStore
 
                 Gamelist.Add(new Game
                 {
-                    Title = "Witcher 3",
+                    Title = "WITCHER 3",
                     Platform = "PC",
                     Condition = "Brandnew",
                     Price = 1999
@@ -98,21 +98,23 @@ namespace LemonStore
                     Console.WriteLine("-----------------------------");
                 }
                 Console.WriteLine("What game to buy bro?");
-                var user_answer = Console.ReadLine();
+                string upperAnswer = Console.ReadLine();
+                string user_Game_choice = upperAnswer.ToUpper();
                 bool searchFlag = false;
                 List<double> gamecart = new List<double>(); //double is used because thats the type of Price
 
-                while (user_answer != "done")
+                while (user_Game_choice != "DONE")
                 {
                     foreach (var gamess in Gamelist)
                     {
-                        if (gamess.Title == user_answer)
+                        if (gamess.Title == user_Game_choice)
                         {
                             Console.WriteLine("Search is successful");
                             Console.WriteLine($"The price is {gamess.Price}");
                             gamecart.Add(gamess.Price);
-                            Console.WriteLine("The game was added to cart, anything else? (gameName/done)");
-                            user_answer = Console.ReadLine();
+                            Console.WriteLine("The game was added to cart, anything else? (GameName/Done)");
+                            string upper = Console.ReadLine();
+                            user_Game_choice = upper.ToUpper();
                             searchFlag = false;
                             break;
                         }
@@ -126,7 +128,8 @@ namespace LemonStore
                         Console.WriteLine("Search is unsuccessful, It seems like we don't have that bro");
                         break;
                     }
-            
+
+
                 }
                 var total_amt = gamecart.Sum();
                 Console.WriteLine($"The total amount to pay is {total_amt}");
@@ -135,55 +138,57 @@ namespace LemonStore
         class Gameconsole : Basedetails
             {
                 public static List<Gameconsole> gameConsoleList = new List<Gameconsole>(); // public static in order to become available globally
-                public static void createConsolelist()
+            public static void createConsolelist()
+            {
+                gameConsoleList.Add(new Gameconsole
                 {
-                    gameConsoleList.Add(new Gameconsole
-                    {
-                        Title = "PS5",
-                        Condition = "Brandnew",
-                        Price = 27650
-                    });
+                    Title = "PS5",
+                    Condition = "Brandnew",
+                    Price = 27650
+                });
 
-                    gameConsoleList.Add(new Gameconsole
-                    {
-                        Title = "PS4",
-                        Condition = "2nd Hand",
-                        Price = 8000
-                    });
+                gameConsoleList.Add(new Gameconsole
+                {
+                    Title = "PS4",
+                    Condition = "2nd Hand",
+                    Price = 8000
+                });
 
-                    gameConsoleList.Add(new Gameconsole
-                    {
-                        Title = "Nintendo Switch",
-                        Condition = "Brandnew",
-                        Price = 14999
-                    });
+                gameConsoleList.Add(new Gameconsole
+                {
+                    Title = "NINTENDO SWITCH",
+                    Condition = "Brandnew",
+                    Price = 14999
+                });
+            }
+            public static void sellGameconsole()
+            {
+                foreach (var game_c in gameConsoleList)
+                {
+                    Console.WriteLine($"Title: {game_c.Title}");
+                    Console.WriteLine($"Item Condition: {game_c.Condition}");
+                    Console.WriteLine($"Price: {game_c.Price}");
+
+                    Console.WriteLine("-----------------------------");
                 }
-                public static void sellGameconsole()
-                {
-                    foreach (var game_c in gameConsoleList)
-                    {
-                        Console.WriteLine($"Title: {game_c.Title}");
-                        Console.WriteLine($"Item Condition: {game_c.Condition}");
-                        Console.WriteLine($"Price: {game_c.Price}");
-
-                        Console.WriteLine("-----------------------------");
-                    }
-                    Console.WriteLine("What game console to buy bro?");
-                var user_gconsole_choice = Console.ReadLine();
+                Console.WriteLine("What game console to buy bro?");
+                string upperAnswer = Console.ReadLine();
+                string user_Gconsole_choice = upperAnswer.ToUpper();
                 bool searchFlag = false;
                 List<double> gameconsolecart = new List<double>();
 
-                while (user_gconsole_choice != "done")
+                while (user_Gconsole_choice != "DONE")
                 {
                     foreach (var game_cc in gameConsoleList)
                     {
-                        if (game_cc.Title == user_gconsole_choice)
+                        if (game_cc.Title == user_Gconsole_choice)
                         {
                             Console.WriteLine("Search is successful");
                             Console.WriteLine($"The price is {game_cc.Price}");
                             gameconsolecart.Add(game_cc.Price);
                             Console.WriteLine("The game console was added to cart, anything else? (ConsoleName/done)");
-                            user_gconsole_choice = Console.ReadLine();
+                            string upper = Console.ReadLine();
+                            user_Gconsole_choice = upper.ToUpper();
                             searchFlag = false;
                             break;
                         }
