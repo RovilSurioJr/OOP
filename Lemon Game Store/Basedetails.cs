786c2b65501c632b182217a7b92e46c7274aa10d
RovilSurioJr <70187981+RovilSurioJr@users.noreply.github.com>
2021-05-15 11:25:19 +0800

    updated some comments

--- Lemon Game Store/Basedetails.cs
+++ Lemon Game Store/Basedetails.cs
@@ -4,8 +4,7 @@ namespace Lemon_Game_Store
 
     abstract class BaseDetails // (Abstraction) -  This a restricted class that cannot be used to create objects (to access it, it must be inherited from another class).
     {
-        // For game details and console details
-        // No need to create these get and set for every class
+        /*For game details and console details, no need to create these get and set for every class on these properties */
         public string Title { get; set; }
         public string Condition { get; set; }
         public double Price { get; set; }
